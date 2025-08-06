72bc1d4719fbf08c2abfc09aa1830334f21583b8
nourkajujutsu <nourkajujutsu@gmail.com>
2025-08-06 20:49:38 +0100

    New Update of code

--- src/main/java/com/openclassrooms/api/controller/EmployeController.java
+++ src/main/java/com/openclassrooms/api/controller/EmployeController.java
@@ -3,9 +3,8 @@ package com.openclassrooms.api.controller;
 import com.openclassrooms.api.model.Employee;
 import com.openclassrooms.api.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeController {
@@ -15,5 +14,23 @@ public class EmployeController {
     @GetMapping("/employees")
     public Iterable<Employee> getEmployees(){
         return employeeService.getEmployees();
+    }
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> setEmployee(@PathVariable Long id,  @RequestBody Employee employeeDetails){
+        try{
+            Employee updateEmplyee = employeeService.setEmployee(id, employeeDetails);
+            return ResponseEntity.ok(updateEmplyee);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @PutMapping
+    public Employee saveEmployee(Employee employee){
+        return employeeService.saveEmployee(employee);
+    }
+    @DeleteMapping("employee/{id}")
+    public ResponseEntity<Employee> deleteEmployee(Long id){
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
     }
 }
