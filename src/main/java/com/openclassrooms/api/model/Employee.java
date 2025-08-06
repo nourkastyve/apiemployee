bb89c8d472833548619a1ca9ff2646d6efd8df02
nourkajujutsu <nourkajujutsu@gmail.com>
2025-08-06 17:06:19 +0100

    Add ALL

---
+++ src/main/java/com/openclassrooms/api/model/Employee.java
@@ -0,0 +1,19 @@
+// Entity representing a User in the DataBase
+package com.openclassrooms.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity // To indicate that the class Employee is a table in the DataBase
+@Table(name = "employees") //To indicate that the class Employee name in the database is employees
+@NoArgsConstructor // Equis by JPA to hydrate the Object from the dataBase
+@AllArgsConstructor // Generates a constructor that takes all fields as parameters.
+@Builder // Pattern Builder for instance creation
+public class Employee {
+    @Id //to indicat that the primary key of the DataBase
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Informs that the attribute will be manager by the database
+    private Long id;
+
+}
